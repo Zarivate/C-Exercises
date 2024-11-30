e80178c756b5f032cd1de66a21d66edb865df7b8
Zarivate <Zarivate@gmail.com>
2024-11-29 23:57:29 -0600

    Had to do minor reconfiguration to get projects working again on local pc

---
+++ SimpleTest/SimpleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy");
+            Console.WriteLine("If you're seeing this that means you got the configuration working.");
+            Console.ReadLine();
+        }
+    }
+}
